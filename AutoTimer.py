72a1e95c31f77d2f2fc4b5ad64842c2b73611f96
vlad <vladghita70@gmail.cpm>
2025-11-12 22:33:36 +0100

    Added handle git and helper methods

--- AutoTimer.py
+++ AutoTimer.py
@@ -1,16 +1,21 @@
-# Use pgrep -l mlagents
+# Use pgrep -l mlagents - DONE
 # Use subprocess
 # Use os.kill SIGINT
 
 import subprocess
 import os
 import signal
-import time
-import git
-from git import Repo, Commit
+import datetime
+from git import Repo
+import pytz
 
 path_to_repo = os.getcwd()
 run_id = 0
+path_to_data = 'data/server'
+timezone = pytz.timezone('Europe/Amsterdam')
+ct = datetime.now(timezone)
+repo = Repo(path_to_repo)
+path_to_training = 'training/linux_train_model.py'
 
 # Method designed to send ^C SIGINT to mlagents process
 def handle_SIGINT():
@@ -25,23 +30,47 @@ def handle_SIGINT():
             pid = int(parts[0])
             os.kill(pid,signal.SIGTERM)
 
+def file_counter(run_id=0):
+    for _,_,files in os.walk(path_to_data):
+        run_id += len(files)
+    return run_id
+
 # Method to handle commiting new .json and run_log data
-#def handle_GIT():
+# TODO: Make method count how many runs are in the result folder and increment run counter based on that
+def handle_GIT():
+    try:
+        for root, dirs, files in os.walk(path_to_data):
+            for file in files:
+                filepath = os.path.relpath(os.path.join(root, file), path_to_repo)
+                repo.index.add([filepath])
+
+        repo.index.commit("Server Auto-Commit: Added data from run number #" + str(run_id))
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 
 
-#def handle_STARTUP():
-    #subprocess.run(['conda','activate','mlagents'])
+# Method to handle conda activate start up and ml agents learn
+def handle_STARTUP():
+    file_counter()
+    subprocess.run(['conda','run','-n','mlagents','python',path_to_training,'--run-id',str(run_id+1)],check=True)
 
 
 
-
+def stop_time_check():
+    now = datetime.datetime.now()
+    return now.hour == 23 and now.minute == 59
 
 
 def main():
-    repo = Repo(path_to_repo)
+
     handle_SIGINT()
+
+    if stop_time_check():
+        handle_SIGINT() # Stops ml-agents learn execute
+        handle_GIT() # Commits and pushes results to repo
+        handle_STARTUP() # Restarts execution
 
     #print(repo.git.status())
 
