9ee23d06bcf894f9ada6ae81b578833a0c5249dd
server <vladghita70@gmail.com>
2025-11-22 20:57:12 +0200

    Modified run #2 results and AutoRunner

--- AutoRunner.sh
+++ AutoRunner.sh
@@ -19,16 +19,19 @@ while true; do
 			trap 'exit' SIGINT
 			conda run --no-capture-output -n mlagents python -u training/linux_train_model.py --run-id $run_id" 
 
-	git add data/server/*.json
+	pkill mlagents-learn # Killing ml agents process 
 
-	# Commit logic
-	if ! git diff --cached --quiet ; then 
-		git commit -m "Server Auto-Commit : Added training results from run #$run_id"
-		git pull --rebase
-		git push
-		echo "Commited results for run_id $run_id after 5 hours"
-	else 
-		echo "No changes to commit for run_id $run_id"
+	if [[ $? -eq 0 ]] ; then # Checks if process is finished, commit logic
+		git add data/server/*.json
+
+		if ! git diff --cached --quiet ; then 
+			git commit -m "Server Auto-Commit : Added training results from run #$run_id"
+			git pull --rebase
+			git push
+			echo "Commited results for run_id $run_id after 5 hours"
+		else 
+			echo "No changes to commit for run_id $run_id"
+		fi
 	fi
 	sleep 10
 done
