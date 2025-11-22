e10de0bb9ade0bb8dfc2aa601f0f6be5655d4a48
server <vladghita70@gmail.com>
2025-11-22 03:24:17 +0200

    Replaced AutoTimer.py with AutoRunner.sh and commited first 5h run

---
+++ AutoRunner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+control_c(){
+    	pkill mlagents-learn
+	if [ -n "$PID" ]; then
+		kill $PID
+	fi
+	exit
+}
+
+trap control_c SIGINT
+
+while true; do 
+	
+
+	run_id=$(($(find data/server -type f | wc -l) + 1))
+
+	timeout --foreground 5h bash -c "
+			trap 'exit' SIGINT
+			conda run --no-capture-output -n mlagents python -u training/linux_train_model.py --run-id $run_id" 
+
+	# Commit logic
+	if ! git diff --cached --quiet ; then 
+		git commit -m "Server Auto-Commit : Added training results from run #$run_id"
+		git pull --rebase
+		git push
+		echo "Commited results for run_id $run_id after 5 hours"
+	else 
+		echo "No changes to commit for run_id $run_id"
+	fi
+	sleep 10
+done
