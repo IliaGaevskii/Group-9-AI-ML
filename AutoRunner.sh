62eac6c41cd52deac65a3382e2e4abe0e486b5b2
server <vladghita70@gmail.com>
2025-11-23 02:21:12 +0200

    Improved AutoRunner

--- AutoRunner.sh
+++ AutoRunner.sh
@@ -17,23 +17,23 @@ while true; do
 
 	run_id=$(($(find data/server -type f | wc -l) + 1))
 
-	timeout --foreground 5h bash -c "
+	timeout --foreground 5m bash -c "
 			trap 'exit' SIGINT
 			conda run --no-capture-output -n mlagents python -u training/linux_train_model.py --run-id $run_id" 
 
-	pkill mlagents-learn # Killing ml agents process 
+	pkill mlagents-learn # Killing ml agents process
 
-	if [[ $? -eq 0 ]] ; then # Checks if process is finished, commit logic
-		git add data/server/*.json
+	sleep 10
 
-		if ! git diff --cached --quiet ; then # checks if any file has been added to staging
-			git commit -m "Server Auto-Commit : Added training results from run #$run_id"
-			git pull --rebase
-			git push
-			echo "Commited results for run_id $run_id after 5 hours"
-		else 
-			echo "No changes to commit for run_id $run_id"
-		fi
+	git add data/server/*.json
+	
+	if ! git diff --cached --quiet ; then # checks if any file has been added to staging
+		git commit -m "Server Auto-Commit : Added training results from run #$run_id"
+		git pull --rebase
+		git push
+		echo "Commited results for run_id $run_id after 5 hours"
+	else 
+		echo "No changes to commit for run_id $run_id"
 	fi
 
 	sleep 10
